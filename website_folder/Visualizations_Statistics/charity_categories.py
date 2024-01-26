44ee7a7252a0c745a7d6bc5a3e6c4ef54c55ca09
stonehenge0 <em.stein29@gmail.com>
2024-01-26 13:42:29 +0100

    Add visualizations for charity categories and charity sources.

---
+++ website_folder/Visualizations_Statistics/charity_categories.py
@@ -0,0 +1,88 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+## This plot visualizes the charity categories and how many charities we have in each section with a circular boxplot. 
+
+
+# Build a test-dataset with pandas
+df = pd.DataFrame(
+         {
+             'Name': ['category ' + str(i) for i in list(range(1, 21)) ],
+             'Value': np.random.randint(low=10, high=100, size=20)
+         })
+
+
+# set figure size
+plt.figure(figsize=(20,10))
+
+# plot polar axis
+ax = plt.subplot(111, polar=True)
+
+# remove grid
+plt.axis('off')
+
+# Set the coordinates limits
+upperLimit = 100
+lowerLimit = 30
+
+# Compute max and min in the dataset
+max = df['Value'].max()
+
+# Computing heights: heights are a conversion of each item value in those new coordinates.
+slope = (max - lowerLimit) / max
+heights = slope * df.Value + lowerLimit
+
+# Compute the width of each bar. In total we have 2*Pi = 360°
+width = 2*np.pi / len(df.index)
+
+# Compute the angle each bar is centered on:
+indexes = list(range(1, len(df.index)+1))
+angles = [element * width for element in indexes]
+angles
+
+
+# initialize the figure
+plt.figure(figsize=(20,10))
+ax = plt.subplot(111, polar=True)
+plt.axis('off')
+
+# Draw bars
+bars = ax.bar(
+    x=angles, 
+    height=heights, 
+    width=width, 
+    bottom=lowerLimit,
+    linewidth=2, 
+    edgecolor="white",
+    color="#61a4b2",
+)
+
+# little space between the bar and the label
+labelPadding = 4
+
+# Add labels
+for bar, angle, height, label in zip(bars,angles, heights, df["Name"]):
+
+    # Labels are rotated. Rotation must be specified in degrees :(
+    rotation = np.rad2deg(angle)
+
+    # Flip some labels upside down
+    alignment = ""
+    if angle >= np.pi/2 and angle < 3*np.pi/2:
+        alignment = "right"
+        rotation = rotation + 180
+    else: 
+        alignment = "left"
+
+    # Finally add the labels
+    ax.text(
+        x=angle, 
+        y=lowerLimit + bar.get_height() + labelPadding, 
+        s=label, 
+        ha=alignment, 
+        va='center', 
+        rotation=rotation, 
+        rotation_mode="anchor")
+    
+plt.show();
