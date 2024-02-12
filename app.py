bc1b28af9a40049b918bf1c1471636153751930f
EtherealCaffeine <150965006+EtherealCaffeine@users.noreply.github.com>
2024-02-12 15:55:57 +0100

    flask works on localhost: All html-pages are accessible, the resultpage shows the values of the selected options, result page added still needs work: try again button, going back to the front page

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
-from flask import Flask, render_template, request, flash
+from flask import Flask, render_template, request
+import json
  
 app = Flask(__name__, static_folder='docs/static', template_folder='docs')
  
-@app.route('/')
-def form():
+@app.route('/' or '/index')
+def index():
+    return render_template('index.html')
+
+
+# questionnaire
+@app.route('/questionnaire.html')
+def questionnaire():
     return render_template('questionnaire.html')
 
-@app.route('/submit_questionnaire', methods = ['POST', 'GET'])
+@app.route('/submit_questionnaire', methods = ['POST'])
 def submit_questionnaire():
     if request.method == 'POST':
-        q_answer2 = request.form['answer2']
-        if not q_answer2 == "Friend":
-            flash('You answered. Yay!')
+        a1 = request.form['answer1']
+        return render_template('result.html', data=request.form)
+    
+@app.route('/try_again')
+def try_again():
+    back_to_q = app.redirect("questionnaire.html", code=302)
+    return back_to_q
+
+# help page
+@app.route('/help.html')
+def help():
+    return render_template('help.html')
 
 
 app.run(host='localhost', port=5000)
