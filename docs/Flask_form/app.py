c0963becf8025ef83e333c04fbad12fc31b93b6d
EtherealCaffeine <150965006+EtherealCaffeine@users.noreply.github.com>
2024-02-12 09:56:08 +0100

    delete html code from app.py

--- docs/Flask_form/app.py
+++ docs/Flask_form/app.py
@@ -15,23 +15,6 @@ def data():
 
         return render_template('data.html',form_data = form_data)
 
-<form>
-  <fieldset>
-    <legend>Please select your preferred contact method:</legend>
-    <div>
-      <input type="radio" id="contactChoice1" name="contact" value="email" />
-      <label for="contactChoice1">Email</label>
-      <input type="radio" id="contactChoice2" name="contact" value="phone" />
-      <label for="contactChoice2">Phone</label>
-      <input type="radio" id="contactChoice3" name="contact" value="mail" />
-      <label for="contactChoice3">Mail</label>
-    </div>
-    <div>
-      <button type="submit">Submit</button>
-    </div>
-  </fieldset>
-</form>
-<pre id="log"></pre>
 
 
 app.run(host='localhost', port=5000)
